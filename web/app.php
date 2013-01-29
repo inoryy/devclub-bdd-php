28ff75548c5a0ae0c17838de9111158fc44fe36b
Roman Marintsenko <inoryy@gmail.com>
2013-01-29 20:32:58 +0200

    implemented enough code to get scenario passing

--- web/app.php
+++ web/app.php
@@ -8,4 +8,12 @@ $app->get('/', function () use ($app) {
     return $app['twig']->render('homepage.html.twig');
 });
 
+$app->post('/search', function () use ($app) {
+    $results = array();
+
+    return $app['twig']->render('homepage.html.twig', array(
+        'results' => $results
+    ));
+})->bind('search_car');
+
 $app->run();
